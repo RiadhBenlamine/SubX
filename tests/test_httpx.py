ec4d1cc4dd77d21206a76e38584c00b893b65699
Riadh Benlamine <riadhriah03@gmail.com>
2026-06-27 15:07:06 +0100

    removed unused deps

--- tests/test_httpx.py
+++ tests/test_httpx.py
@@ -1,10 +1,10 @@
 # probe_test.py
 import asyncio
-from tools.httpx import (Httpx)
+from tools.httpx import HttpxTool
 
 async def main():
-    prober = Httpx("hackerone.com")
-    results = await prober.run_httpx()
+    prober = HttpxTool()
+    results = await prober.run(["hackerone.com"])
     print(f"Got {len(results)} results")
     for r in results:
         print(r)
