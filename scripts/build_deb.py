478e9737db25e39e0c5950c63c7908c32a7e0aaf
Riadh Benlamine <riadhriah03@gmail.com>
2026-08-09 20:48:35 +0100

    bump: version 2.1.2

--- scripts/build_deb.py
+++ scripts/build_deb.py
@@ -11,7 +11,7 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).resolve().parent.parent
 DIST_DIR = ROOT_DIR / "dist"
 BUILD_DIR = ROOT_DIR / "build_deb"
-VERSION = "2.1.1"
+VERSION = "2.1.2"
 PACKAGE_NAME = "subx"
 DEB_FILENAME = f"{PACKAGE_NAME}_{VERSION}-1_all.deb"
 
